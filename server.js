2f55c743a06795a9844beca4ee193271e6da0a59
Yashdeep Shetty <yashdeep@clevertap.com>
2025-03-09 12:56:58 +0530

    Adding all files

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const io = new Server(8080,{
 })
 
 io.on('connection', (socket) => {
+    console.log(socket.id)
     console.log('someone connected!');
     socket.on('message',message => {
         console.log('Message: '+ message)
