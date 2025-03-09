13fa6671b2038d3364612efe17bea3ab946af97b
Yashdeep Shetty <yashdeep@clevertap.com>
2025-03-09 12:31:24 +0530

    Adding all files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+// Socket Logic
+const http = require('http')
+const { Server } = require("socket.io")
+const server = http.createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('someone connected!');
+    socket.on('message',message => {
+        console.log('Message: '+ message)
+        socket.broadcast.emit('message',message)
+    })
+    socket.on('disconnect',reason => {
+        console.log('User disconnected : '+ reason)
+    })
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/index.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
